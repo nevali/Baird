e3cced1a74490259921a62a257a9b5b64bbbae81
Mo McRoberts <mo@toybox.nexgenta.com>
2010-08-19 09:00:38 +0000

    dvb: service URI matching, et al.

--- whatthecrid/index.php
+++ whatthecrid/index.php
@@ -2,48 +2,44 @@
 
 require_once(dirname(__FILE__) . '/../platform/lib/common.php');
 require_once(dirname(__FILE__) . '/config.php');
+require_once(dirname(__FILE__) . '/services.php');
+
+function dvbMatch($onid, $tsid = null, $sid = null, $nid = null, $list = null)
+{
+	global $services;
+	
+	if($list === null) $list = $services;
+	foreach($list as $uri => $srv)
+	{
+		if(isset($srv['dvb']))
+		{
+			foreach($srv['dvb'] as $dvb)
+			{
+				if(strcasecmp($dvb['o'], $onid)) continue;
+				if($sid !== null)
+				{
+					if(strcasecmp($dvb['t'], $tsid)) continue;
+					if(strcasecmp($dvb['s'], $sid)) continue;
+				}
+				return $uri;
+			}
+		}
+		else if(isset($srv['children']))
+		{
+			$uri = dvbMatch($onid, $tsid, $sid, $nid, $srv['children']);
+			if(strlen($uri))
+			{
+				return $uri;
+			}
+		}
+	}
+	return null;
+}
+
 
 $endpoint = NOTUBE_QUERY_URL;
 
 // The list of service identifiers that we have responsibility for resolving for
-
-$services = array(
-	'http://www.bbc.co.uk/services/bbcone#service' => array(
-		'name' => 'bbcone',
-		'dvb' => array(),
-		'children' => array(
-			'http://www.bbc.co.uk/services/bbcone/west#service' => array(
-				'name' => 'west',
-				'dvb' => array(
-					array('n' => '3098', 's' => '1041', 't' => '1041', 'o' => '233a'),
-				),
-			),
-			'http://www.bbc.co.uk/services/bbcone/south_west#service' => array(
-				'name' => 'south_west',
-			),
-			'http://www.bbc.co.uk/services/bbcone/london#service' => array(
-				'name' => 'london',
-			),
-			'http://www.bbc.co.uk/services/bbcone/west_midlands#service' => array(
-				'name' => 'west_midlands',
-			),
-			'http://www.bbc.co.uk/services/bbcone/east_midlands#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/east#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/north_west#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/north_east#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/yorkshire#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/oxford#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/south_east#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/channel_islands#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/east_yorkshire#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/scotland#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/wales#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/ni#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/south#service' => array(),
-			'http://www.bbc.co.uk/services/bbcone/cambridge#service' => array(),
-		),
-	),
-);
 
 $service = null;
 $start = null;
@@ -123,7 +119,11 @@ if(isset($_GET['uri']))
 		foreach($uris as $uristr)
 		{
 			$match = array();
-			if(preg_match('/^([a-z0-9-+]+):/i', $uristr, $match) && count($match) >= 2)
+			if(preg_match('/^tag:feeds.bbc.co.uk,2008:PIPS:([a-z0-9]+)$/i', $uristr, $match) && count($match) >= 2)
+			{
+				$uri['pid'][] = 'pid:' . $match[1];
+			}
+			else if(preg_match('/^([a-z0-9-+]+):/i', $uristr, $match) && count($match) >= 2)
 			{
 				$uri[$match[1]][] = $uristr;
 			}
@@ -140,30 +140,73 @@ if(isset($_GET['start']))
 	$start = $_GET['start'];
 }
 
-if(!$service)
+if(!strlen($service) && isset($uri['dvb']))
 {
-	die('no service');
+	foreach($uri['dvb'] as $u)
+	{
+		$dmatch = array();
+		$onid = $tsid = $sid = null;
+		if(preg_match('!^dvb://([a-f0-9]{1,4})(.([a-f0-9]{1,4})(.([a-f0-9]{1,4}))?)?([.$/;~].*)?$!i', $u, $dmatch))
+		{
+			if(isset($dmatch[1]))
+			{
+				$onid = $dmatch[1];
+				if(isset($dmatch[3]) && isset($dmatch[5]))
+				{
+					$tsid = $dmatch[3];
+					$sid = $dmatch[5];
+				}
+			}
+		}
+		$service = dvbMatch($onid, $tsid, $sid);
+		if(strlen($service))
+		{
+			break;
+		}
+	}
 }
 
-$queryParams = array('?version' => true, '?broadcast' => true);
+$queryParams = array('?broadcast' => true);
 $where = array();
 
-$where[] = '?broadcast <http://purl.org/ontology/po/broadcast_on> <' . $service . '>';
+if(strlen($service))
+{
+	$where[] = '?broadcast <http://purl.org/ontology/po/broadcast_on> <' . $service . '>';
+}
+
+$queryParams['?service'] = true;
+$where[] = '?broadcast <http://purl.org/ontology/po/broadcast_on> ?service';
+
+$queryParams['?version'] = true;
 $where[] = '?broadcast <http://purl.org/ontology/po/broadcast_of> ?version';
+
+$queryParams['?prog'] = true;
+$where[] = 'OPTIONAL { ?prog <http://purl.org/ontology/po/version> ?version }';
 
 $queryParams['?crid'] = true;
 $where[] = 'OPTIONAL { ?crid <http://www.w3.org/2002/07/owl#sameAs> ?version . FILTER regex(str(?crid), "^crid://", "i") }';
 
+$n = 0;
 if(isset($uri['crid']))
 {
 	$list = array();
-	$n = 0;
 	foreach($uri['crid'] as $crid)
 	{
 		$queryParams['?uri' . $n] = true;
 		$list[] = '{ ?uri' . $n . ' <http://www.w3.org/2002/07/owl#sameAs> ?version . FILTER regex(str(?uri' . $n . '), "' . $crid . '", "i") }';
 	}
 	$where[] = implode(' UNION ', $list);
+}
+if(isset($uri['pid']))
+{
+	$list = array();
+	foreach($uri['pid'] as $tag)
+	{
+		$tag = substr($tag, 4);
+		$queryParams['?uri' . $n] = true;
+		$where[] = 'FILTER regex(str(?version), "http://www.bbc.co.uk/programmes/' . $tag . '#programme", "i")';
+//		$list[] = '{ ?uri' . $n . ' <http://www.w3.org/2002/07/owl#sameAs> ?broadcast . FILTER regex(str(?uri' . $n . '), "' . $tag . '", "i") }';
+	}
 }
 
 if(strlen($start))
@@ -179,24 +222,22 @@ if(strlen($start))
 /* Construct a SPARQL query from our assorted parameters */
 if(count($queryParams))
 {
-	$query = 'SELECT ' . implode(' ', array_keys($queryParams)) . ' WHERE { ' . implode(' . ', $where) . ' }';
+	$query = 'SELECT ' . implode(' ', array_keys($queryParams)) . ' WHERE { ' . implode(' . ', $where) . ' } LIMIT 1';
 }
 else
 {
 	die('No query parameters');
 }
 
-header('Content-type: text/plain');
-
-// echo $query . "\n";
-
 $result = json_decode(file_get_contents($endpoint . '?query=' . urlencode($query)), true);
 $target = null;
 $crid = null;
+$match = null;
 if($result[0] == '200' && isset($result[1]) && is_array($result[1]))
 {
 	foreach($result[1] as $res)
-	{
+	{		
+		$match = $res;
 		if(isset($res['broadcast']))
 		{
 			$target = $res['broadcast'];
@@ -205,9 +246,9 @@ if($result[0] == '200' && isset($result[1]) && is_array($result[1]))
 		{
 			$target = $res['version'];
 		}
-		else if(isset($res['p']))
+		else if(isset($res['prog']))
 		{
-			$target = $res['p'];
+			$target = $res['prog'];
 		}
 		if(isset($res['crid']))
 		{
@@ -221,9 +262,86 @@ if(isset($_GET['kind']))
 	switch($_GET['kind'])
 	{
 		case 'crid':
+			header('Content-type: text/plain');
 			die($crid);
+		case 'results':
+			header('Content-type: text/plain');
+			print_r($result);
+			die();
+		case 'json':
+			header('Content-type: application/json');		
+			echo json_encode($result);
+			die();
 	}
 }
-print_r($result);
-die($target);
 
+function e()
+{
+	$x = func_get_args();
+	echo htmlspecialchars(implode(' ', $x));
+}
+
+function _e()
+{
+	$x = func_get_args();
+	return htmlspecialchars(implode(' ', $x));
+}
+
+
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>What the crid…?!</title>
+	</head>
+	<body>
+		<h1><?php if($target) { e($target); } else { e('No match'); }?></h1>
+		<?php if(!empty($_GET['showQuery']))
+		{
+			echo '<p><code>' . _e($query) . '</code></p>';
+		}
+		?>
+		<dl>
+		<?php if(isset($match['service']))
+		{
+			echo '<dt>Service:</dt>';
+			echo '<dd>&lt;<a href="' . _e($match['service']) . '">' . _e($match['service']) . '</a>&gt;</dd>';
+		} ?>
+		<?php if(isset($match['prog']))
+		{
+			echo '<dt>Episode:</dt>';
+			echo '<dd>&lt;<a href="' . _e($match['prog']) . '">' . _e($match['prog']) . '</a>&gt;</dd>';
+		} ?>
+		<?php if(isset($match['version']))
+		{
+			echo '<dt>Version:</dt>';
+			echo '<dd>&lt;<a href="' . _e($match['version']) . '">' . _e($match['version']) . '</a>&gt;</dd>';
+		} ?>
+		<?php if(isset($match['broadcast']))
+		{
+			echo '<dt>Broadcast:</dt>';
+			echo '<dd>&lt;<a href="' . _e($match['broadcast']) . '">' . _e($match['broadcast']) . '</a>&gt;</dd>';
+		} ?>
+		<?php if(isset($match['crid']))
+		{
+			echo '<dt>pCRID:</dt>';
+			echo '<dd>&lt;<a href="' . _e($match['crid']) . '">' . _e($match['crid']) . '</a>&gt;</dd>';
+		} ?>
+		</dl>
+		<ul>
+		<?php if(isset($match['prog']))
+		{
+			echo '<li><a href="http://atlasapi.org/2.0/items.html?uri=' . _e($match['prog']) . '">Find media using Atlas</a></li>';
+			$matches = array();
+			if(preg_match('!^http://www.bbc.co.uk/programmes/([a-z0-9]+)#!i', $match['prog'], $matches) && count($matches) >= 2)
+			{
+				echo '<li><a href="http://www.bbc.co.uk/iplayer/episode/' . $matches[1] . '">Watch on iPlayer</a> (if available)</li>';
+			}
+		} ?>
+		<?php if(isset($match['crid']))
+		{
+			echo '<li><a href="http://g.bbcredux.com/search?repeats=on&pcrid=' . _e($match['crid']) . '">Find media using Redux</a> (BBC staff only)</li>';
+		} ?>
+		</ul>
+	</body>
+</html>
---
+++ whatthecrid/services.php
@@ -0,0 +1,126 @@
+<?php
+
+$services = array(
+	'http://www.bbc.co.uk/services/bbcone#service' => array(
+		'name' => 'bbcone',
+		'children' => array(
+			'http://www.bbc.co.uk/services/bbcone/west#service' => array(
+				'name' => 'west',
+				'dvb' => array(
+					array('n' => '3098', 's' => '1041', 't' => '1041', 'o' => '233a'),
+				),
+			),
+			'http://www.bbc.co.uk/services/bbcone/south_west#service' => array(
+				'name' => 'south_west',
+			),
+			'http://www.bbc.co.uk/services/bbcone/london#service' => array(
+				'name' => 'london',
+			),
+			'http://www.bbc.co.uk/services/bbcone/west_midlands#service' => array(
+				'name' => 'west_midlands',
+			),
+			'http://www.bbc.co.uk/services/bbcone/east_midlands#service' => array(
+				'name' => 'east_midlands',
+			),
+			'http://www.bbc.co.uk/services/bbcone/east#service' => array(
+				'name' => 'east',
+			),
+			'http://www.bbc.co.uk/services/bbcone/north_west#service' => array(
+				'name' => 'north_west',
+			),
+			'http://www.bbc.co.uk/services/bbcone/north_east#service' => array(
+				'name' => 'north_east',
+			),
+			'http://www.bbc.co.uk/services/bbcone/yorkshire#service' => array(
+				'name' => 'yorkshire',
+			),
+			'http://www.bbc.co.uk/services/bbcone/oxford#service' => array(
+				'name' => 'oxford',
+			),
+			'http://www.bbc.co.uk/services/bbcone/south_east#service' => array(
+				'name' => 'south_east',
+			),
+			'http://www.bbc.co.uk/services/bbcone/channel_islands#service' => array(
+				'name' => 'channel_islands',
+			),
+			'http://www.bbc.co.uk/services/bbcone/east_yorkshire#service' => array(
+				'name' => 'east_yorkshire',
+			),
+			'http://www.bbc.co.uk/services/bbcone/scotland#service' => array(
+				'name' => 'scotland',
+			),
+			'http://www.bbc.co.uk/services/bbcone/wales#service' => array(
+				'name' => 'wales',
+			),
+			'http://www.bbc.co.uk/services/bbcone/ni#service' => array(
+				'name' => 'ni',
+			),
+			'http://www.bbc.co.uk/services/bbcone/south#service' => array(
+				'name' => 'south',
+			),
+			'http://www.bbc.co.uk/services/bbcone/cambridge#service' => array(
+				'name' => 'cambridge',
+			),
+		),
+	),
+	'http://www.bbc.co.uk/services/bbctwo#service' => array(
+		'name' => 'bbctwo',
+		'children' => array(
+			'http://www.bbc.co.uk/services/bbctwo/england#service' => array(
+				'name' => 'england',
+				'dvb' => array(
+				      array('n' => '3098', 's' => '10bf', 't' => '1041', 'o' => '233a'),
+				),
+			),
+			'http://www.bbc.co.uk/services/bbctwo/scotland#service' => array(
+				'name' => 'scotland',
+			),
+			'http://www.bbc.co.uk/services/bbctwo/ni#service' => array(
+				'name' => 'ni',
+			),
+			'http://www.bbc.co.uk/services/bbctwo/wales#service' => array(
+				'name' => 'wales',
+			),
+		),
+	),
+	'http://www.bbc.co.uk/services/bbcthree#service' => array(
+		'name' => 'bbcthree',
+		'dvb' => array(
+		      array('n' => '3098', 's' => '10c0', 't' => '1041', 'o' => '233a'),
+		),
+	),
+	'http://www.bbc.co.uk/services/bbcnews#service' => array(
+		'name' => 'bbcnews',
+		'dvb' => array(
+		      array('n' => '3098', 's' => '1100', 't' => '1041', 'o' => '233a'),
+		),
+	),
+	'http://www.bbc.co.uk/services/bbcredbutton#service' => array(
+		'name' => 'bbcredbutton',
+		'dvb' => array(
+		      array('n' => '3098', 's' => '1140', 't' => '1041', 'o' => '233a'),
+		),
+	),
+	'http://www.bbc.co.uk/services/bbcfour#service' => array(
+		'name' => 'bbcfour',
+		'dvb' => array(
+		      array('n' => '3098', 's' => '11c0', 't' => '1041', 'o' => '233a'),
+		),
+	),
+	'http://www.bbc.co.uk/services/cbbc#service' => array(
+		'name' => 'cbbc',
+		'dvb' => array(
+		      array('n' => '3098', 's' => '1200', 't' => '1041', 'o' => '233a'),
+		),
+	),
+	'http://www.bbc.co.uk/services/bbcparliament#service' => array(
+		'name' => 'bbcparliament',
+		'dvb' => array(
+		      array('n' => '3098', 's' => '1280', 't' => '1041', 'o' => '233a'),
+		),
+	),
+	'http://www.bbc.co.uk/services/cbeebies#service' => array(
+		'name' => 'cbeebies',
+	),
+
+);
